31e3abf477f023cdabbb8cd14902c4753324bbc0
2019331013 <asanulhaquekiron@gmail.com>
2023-10-15 08:43:02 +0600

    major fuction added

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import * as styles from "../styles/ServiceCard.module.css";
+import Link from "next/link";
+
 const ServiceCard = ({ service }) => {
 	return (
+
 		<div className={styles.cardContainer}>
-			<div className={`${styles.imageContainer}`}>
-				<div>
-					<Image src={service.picture} alt="Health is Wealth" />
+			<Link href={`${service?.url}`}>
+				<div className={`${styles.imageContainer}`}>
+					<div>
+						<Image src={service.picture} alt="Health is Wealth" />
+					</div>
 				</div>
-			</div>
-			<h2 className={`${styles.cardHeading}`}>{service.title}</h2>
-			<p className={`${styles.cardDes} text-gray`}>
-				{service.description}
-			</p>
+				<h2 className={`${styles.cardHeading}`}>{service.title}</h2>
+				<p className={`${styles.cardDes} text-gray`}>
+					{service.description}
+				</p>
+			</Link>
 		</div>
 	);
 };

deb654394ef066ee3a6511abaad80d4a53833fb6
Sami-63 <rssami.bd@gmail.com>
2023-10-15 06:32:58 +0600

    blogs and blog

--- pages/blogs.jsx
+++ pages/blogs.jsx
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+import Articles from "../components/Articles";
+import data from "../constants/data";
+
 const blogs = () => {
   return (
     <div>
@@ -8,7 +11,7 @@ const blogs = () => {
       <div className='flex flex-col items-center justify-center'>
         <h1 className='text-5xl font-bold text-[#FF4A4A] my-4'>Blog</h1>
         <div>
-          
+          <Articles articles={data.articles} />
         </div>
       </div>
       <Footer />
